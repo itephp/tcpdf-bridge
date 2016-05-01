b69662aa6c446862958fbf378dad43f6e072dcdc
Michał Tomczak <michal@tomczak.net.pl>
2016-05-01 15:50:06 +0200

    Init module.

---
+++ src/Service.php
@@ -0,0 +1,27 @@
+<?php
+
+/**
+ * ItePHP: Framework PHP (http://itephp.com)
+ * Copyright (c) NewClass (http://newclass.pl)
+ *
+ * Licensed under The MIT License
+ * For full copyright and license information, please see the file LICENSE
+ * Redistributions of files must retain the above copyright notice.
+ *
+ * @copyright     Copyright (c) NewClass (http://newclass.pl)
+ * @link          http://itephp.com ItePHP Project
+ * @license       http://www.opensource.org/licenses/mit-license.php MIT License
+ */
+namespace ItePHP\TCPDF;
+
+class Service{
+	
+	public function __construct(ServiceConfig $serviceConfig,EventManager $eventManager){
+
+	}
+
+	public function create(){
+		return new \TCPDI(PDF_PAGE_ORIENTATION, PDF_UNIT, PDF_PAGE_FORMAT, true, 'UTF-8', false);
+	}
+
+}
